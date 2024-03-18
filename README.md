5599a13f8bd9e8342dec01edee309bfa2a6bb9ea
sichao Y <13816901408@163.com>
2024-03-18 21:36:48 +0800

    feat: add reset and cfg file

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # sTimer
 
+sTimer is a timer pomotroid with minimum feature. It takes config from `cfg.ini` file and run with command `python stimer.py`.
 
+To install dependency, run `pip install -r requirement`.
 
+You can customize the alarm sound by replacing the default mp3 and trim time of the alarm:
 `ffmpeg -i battleship-alarm.wav -t 2 alarm.mp3`
--- stimer.py
+++ stimer.py
@@ -1,18 +1,14 @@
 from tkinter import *
 from playsound import playsound
 import time
-import multiprocessing as mp
 from tkinter import ttk
 import threading
-root = Tk()
-root.title("sTimer")
-root.geometry("400x160")
-root.config(bg="#000")
-root.resizable(False, False)
-root.wm_attributes("-topmost", 1)
+import configparser
 
-COOLDOWN = 1 * 10
-FOCUS = 1 * 10
+config = configparser.ConfigParser()
+config.read("cfg.ini")
+FOCUS = eval(config.get("top", "focus_time"))
+COOLDOWNS = eval(config.get("top", "short_cooldown_time"))
 
 
 def background(func, args):
@@ -24,9 +20,9 @@ def add_leading_zero(number):
     return "{:02d}".format(number)
 
 
-def Focus():
+def Reset():
     global STOP
-    STOP = 2
+    STOP = True
     hr = FOCUS // (60 * 60)
     min = FOCUS // 60
     sec = FOCUS % 60
@@ -43,10 +39,12 @@ def set_pbar():
 
 def Timer(countdown):
     global STOP
-    STOP = 0
+    STOP = False
+    if countdown == 0:
+        return
     pbar = set_pbar()
     incre_amount = 100 / countdown
-    while countdown > 0 and not STOP:
+    while countdown >= 0 and not STOP:
         minute, second = (countdown // 60, countdown % 60)
         hour = 0
         if minute > 60:
@@ -66,48 +64,81 @@ def _playsound(filepath):
     background(playsound, (filepath,))
 
 
-def Start(cooldown=True, cooldown_time=COOLDOWN):
+def Start(cooldown=True, cooldown_time=COOLDOWNS):
     countdown = int(hrs.get()) * 3600 + int(mins.get()) * 60 + int(secs.get())
     Timer(countdown)
     if not STOP:
         _playsound("alarm.mp3")
         if cooldown:
             Timer(cooldown_time)
-    if STOP != 2:
-        secs.set("00")
-        mins.set("00")
-        hrs.set("00")
 
 
 def Stop():
     global STOP
-    STOP = 1
+    STOP = True
 
+
+root = Tk()
+root.title("sTimer")
+root.geometry("400x160")
+root.config(bg="#000")
+root.resizable(False, False)
+root.wm_attributes("-topmost", 1)
 
 # timer
 timer_y = 20
 unit_y = timer_y + 40
 hrs = StringVar()
-Entry(root, textvariable=hrs, width=2, font="arial 50", bg="#001", fg="#fff", bd=0).place(x=30, y=timer_y)
-hrs.set("00")
+Entry(
+    root, textvariable=hrs, width=2, font="arial 50", bg="#001", fg="#fff", bd=0
+).place(x=30, y=timer_y)
+hrs.set(add_leading_zero(FOCUS // (60 * 60)))
 mins = StringVar()
-Entry(root, textvariable=mins, width=2, font="arial 50", bg="#001", fg="#fff", bd=0).place(x=150, y=timer_y)
-mins.set("00")
+Entry(
+    root, textvariable=mins, width=2, font="arial 50", bg="#001", fg="#fff", bd=0
+).place(x=150, y=timer_y)
+mins.set(add_leading_zero(FOCUS // (60)))
 secs = StringVar()
-Entry(root, textvariable=secs, width=2, font="arial 50", bg="#001", fg="#fff", bd=0).place(x=270, y=timer_y)
-secs.set("00")
+Entry(
+    root, textvariable=secs, width=2, font="arial 50", bg="#001", fg="#fff", bd=0
+).place(x=270, y=timer_y)
+secs.set(add_leading_zero(FOCUS % 60))
 Label(root, text="hour", font="arial 15", bg="#000", fg="#fff").place(x=105, y=unit_y)
 Label(root, text="min", font="arial 15", bg="#000", fg="#fff").place(x=225, y=unit_y)
 Label(root, text="sec", font="arial 15", bg="#000", fg="#fff").place(x=345, y=unit_y)
 
 buttonStart = Button(
-    root, text="Start", bg="#ea3548", bd=0, fg="#fff", width=10, height=2, font="arial 10 bold", command=Start
+    root,
+    text="Start",
+    bg="#ea3548",
+    bd=0,
+    fg="#fff",
+    width=10,
+    height=2,
+    font="arial 10 bold",
+    command=Start,
 )
 buttonStop = Button(
-    root, text="Stop", bg="#ea3548", bd=0, fg="#fff", width=10, height=2, font="arial 10 bold", command=Stop
+    root,
+    text="Stop",
+    bg="#ea3548",
+    bd=0,
+    fg="#fff",
+    width=10,
+    height=2,
+    font="arial 10 bold",
+    command=Stop,
 )
 buttonFocus = Button(
-    root, text="Focus", bg="#ea3548", bd=0, fg="#fff", width=10, height=2, font="arial 10 bold", command=Focus
+    root,
+    text="Reset",
+    bg="#ea3548",
+    bd=0,
+    fg="#fff",
+    width=10,
+    height=2,
+    font="arial 10 bold",
+    command=Reset,
 )
 button_y = timer_y + 80
 xs = [30, 155, 280]
