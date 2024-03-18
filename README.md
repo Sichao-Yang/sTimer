81891a3fd46129032e64458f513651e2d15ef5f9
SichaoY <13816901408@163.com>
2024-03-18 18:42:25 +0800

    feat: finished main target

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sTimer
+
+
+
+`ffmpeg -i battleship-alarm.wav -t 2 alarm.mp3`
---
+++ timer.py
@@ -0,0 +1,119 @@
+from tkinter import *
+from playsound import playsound
+import time
+import multiprocessing as mp
+from tkinter import ttk
+import threading
+root = Tk()
+root.title("sTimer")
+root.geometry("400x160")
+root.config(bg="#000")
+root.resizable(False, False)
+root.wm_attributes("-topmost", 1)
+
+COOLDOWN = 1 * 10
+FOCUS = 1 * 10
+
+
+def background(func, args):
+    th = threading.Thread(target=func, args=args)
+    th.start()
+
+
+def add_leading_zero(number):
+    return "{:02d}".format(number)
+
+
+def Focus():
+    global STOP
+    STOP = 2
+    hr = FOCUS // (60 * 60)
+    min = FOCUS // 60
+    sec = FOCUS % 60
+    hrs.set(add_leading_zero(hr))
+    mins.set(add_leading_zero(min))
+    secs.set(add_leading_zero(sec))
+
+
+def set_pbar():
+    pbar = ttk.Progressbar()
+    pbar.place(x=50, y=0, width=300)
+    return pbar
+
+
+def Timer(countdown):
+    global STOP
+    STOP = 0
+    pbar = set_pbar()
+    incre_amount = 100 / countdown
+    while countdown > 0 and not STOP:
+        minute, second = (countdown // 60, countdown % 60)
+        hour = 0
+        if minute > 60:
+            hour, minute = (minute // 60, minute % 60)
+        secs.set(add_leading_zero(second))
+        mins.set(add_leading_zero(minute))
+        hrs.set(add_leading_zero(hour))
+        root.update()
+        time.sleep(1)
+        countdown -= 1
+        pbar.step(incre_amount)
+    pbar.destroy()
+
+
+def _playsound(filepath):
+    # playsound(filepath)
+    background(playsound, (filepath,))
+
+
+def Start(cooldown=True, cooldown_time=COOLDOWN):
+    countdown = int(hrs.get()) * 3600 + int(mins.get()) * 60 + int(secs.get())
+    Timer(countdown)
+    if not STOP:
+        _playsound("alarm.mp3")
+        if cooldown:
+            Timer(cooldown_time)
+    if STOP != 2:
+        secs.set("00")
+        mins.set("00")
+        hrs.set("00")
+
+
+def Stop():
+    global STOP
+    STOP = 1
+
+
+# timer
+timer_y = 20
+unit_y = timer_y + 40
+hrs = StringVar()
+Entry(root, textvariable=hrs, width=2, font="arial 50", bg="#001", fg="#fff", bd=0).place(x=30, y=timer_y)
+hrs.set("00")
+mins = StringVar()
+Entry(root, textvariable=mins, width=2, font="arial 50", bg="#001", fg="#fff", bd=0).place(x=150, y=timer_y)
+mins.set("00")
+secs = StringVar()
+Entry(root, textvariable=secs, width=2, font="arial 50", bg="#001", fg="#fff", bd=0).place(x=270, y=timer_y)
+secs.set("00")
+Label(root, text="hour", font="arial 15", bg="#000", fg="#fff").place(x=105, y=unit_y)
+Label(root, text="min", font="arial 15", bg="#000", fg="#fff").place(x=225, y=unit_y)
+Label(root, text="sec", font="arial 15", bg="#000", fg="#fff").place(x=345, y=unit_y)
+
+buttonStart = Button(
+    root, text="Start", bg="#ea3548", bd=0, fg="#fff", width=10, height=2, font="arial 10 bold", command=Start
+)
+buttonStop = Button(
+    root, text="Stop", bg="#ea3548", bd=0, fg="#fff", width=10, height=2, font="arial 10 bold", command=Stop
+)
+buttonFocus = Button(
+    root, text="Focus", bg="#ea3548", bd=0, fg="#fff", width=10, height=2, font="arial 10 bold", command=Focus
+)
+button_y = timer_y + 80
+xs = [30, 155, 280]
+buttonFocus.place(x=xs[0], y=button_y)
+buttonStart.place(x=xs[1], y=button_y)
+buttonStop.place(x=xs[2], y=button_y)
+
+
+root.mainloop()
