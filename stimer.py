6fb925312575ab978bd198c10ea29b858fb8f4c2
SichaoY <13816901408@163.com>
2024-03-20 14:37:31 +0800

    feat: add repetition and state memory

--- stimer.py
+++ stimer.py
@@ -40,15 +40,32 @@ class sTimer:
             config.read("cfg.ini")
         else:
             config.read(config_path)
-        self.focus_time = int(eval(config.get("top", "focus_time")))
-        self.short_cooldown = int(eval(config.get("top", "short_cooldown_time")))
+        self.focus_time = self.conv_int(config.get("top", "focus_time"))
+        self.short_cooldown = self.conv_int(config.get("top", "short_cooldown_time"))
         self.tick = config.getboolean("top", "tick")
         self.cooldown = cooldown
         self.sound_path_work = config.get("top", "sound_path_work")
         self.sound_path_short_cooldown = config.get("top", "sound_path_short_cooldown")
         self.sound_path_tick = config.get("top", "sound_path_tick")
         self.window_position = config.get("top", "window_position")
-        self.stop = False
+        self.total_rep = self.conv_int(config.get("top", "repetition"))
+        self.current_rep = 0
+        self.timer_active_timer = False
+        self.loop_state = "focus"
+
+    def conv_int(self, str):
+        return int(eval(str))
+
+    def _format_rep(self):
+        return f"{self.current_rep}/{self.total_rep}"
+
+    def _reset_rep(self):
+        self.current_rep = 0
+        return self._format_rep()
+
+    def _incr_rep(self):
+        self.current_rep += 1
+        return self._format_rep()
 
     def run(self):
         self.root = Tk()
@@ -95,6 +112,13 @@ class sTimer:
         )
         self.secs.set(add_leading_zero(self.focus_time % 60))
 
+        # repetition
+        self.rep_count = StringVar()
+        Entry(
+            self.root, textvariable=self.rep_count, width=3, font=("Arial Black", 10, ""), bg="#001", fg="#fff", bd=0
+        ).place(x=5, y=timer_y - 10)
+        self.rep_count.set(self._format_rep())
+        # control button
         Font_tuple = ("Arial", 15, "")
         Label(self.root, text="hr", font=Font_tuple, bg="#000", fg="#fff").place(x=105, y=unit_y)
         Label(self.root, text="min", font=Font_tuple, bg="#000", fg="#fff").place(x=225, y=unit_y)
@@ -141,11 +165,28 @@ class sTimer:
 
         self.root.mainloop()
 
+    @property
+    def timer_active(self):
+        return self._active
+
+    @timer_active.setter
+    def timer_active(self, value):
+        self._active = value
+
+    def start_timer(self):
+        self.timer_active = True
+
     def Stop(self):
-        self.stop = True
+        self.timer_active = False
 
     def Reset(self):
-        self.stop = True
+        self.Stop()
+        self._reset_timer()
+        self.rep_count.set(self._reset_rep())
+        # force loop state back to focus
+        self.loop_state = "focus"
+
+    def _reset_timer(self):
         hr = self.focus_time // (60 * 60)
         min = self.focus_time // 60
         sec = self.focus_time % 60
@@ -154,22 +195,33 @@ class sTimer:
         self.secs.set(add_leading_zero(sec))
 
     def Start(self):
-        countdown = int(self.hrs.get()) * 3600 + int(self.mins.get()) * 60 + int(self.secs.get())
-        _playsound(self.sound_path_work)
-        self.Timer(countdown, self.tick)
-        if not self.stop:
-            _playsound(self.sound_path_short_cooldown)
-            if self.cooldown:
-                # dont play tick sound while cooldown
-                self.Timer(self.short_cooldown, False)
+        self.start_timer()
+        while (self.current_rep < self.total_rep) and self.timer_active:
+            countdown = int(self.hrs.get()) * 3600 + int(self.mins.get()) * 60 + int(self.secs.get())
+            if self.loop_state == "focus":
+                _playsound(self.sound_path_work)
+                self.Timer(countdown, self.tick)
+                if self.timer_active:
+                    self.loop_state = "cooldown"
+                    _playsound(self.sound_path_short_cooldown)
+                    countdown = self.short_cooldown
+            if self.loop_state == "cooldown":
+                if self.cooldown:
+                    # dont play tick sound while cooldown
+                    self.Timer(countdown, False)
+            # only when the timer finished without stop signal, the following will execute
+            if self.timer_active:
+                self._incr_rep()
+                self.rep_count.set(self._format_rep())
+                self._reset_timer()
+                self.loop_state = "focus"
 
     def Timer(self, countdown, tick):
-        self.stop = False
         if countdown == 0:
             return
         pbar = set_pbar()
         incr_amount = 100 / countdown
-        while countdown >= 0 and not self.stop:
+        while (countdown >= 0) and (self.timer_active):
             minute, second = (countdown // 60, countdown % 60)
             hour = 0
             if minute > 60:
@@ -178,11 +230,11 @@ class sTimer:
             self.mins.set(add_leading_zero(minute))
             self.hrs.set(add_leading_zero(hour))
             self.root.update()
-            time.sleep(1)
+            pbar.step(incr_amount)
             if tick:
                 _playsound(self.sound_path_tick)
-            pbar.step(incr_amount)
             countdown -= 1
+            time.sleep(1)
         pbar.destroy()
 
 
@@ -192,4 +244,7 @@ def run():
 
 
 if __name__ == "__main__":
+    # st = sTimer()
+    # st.stop = True
+    # print(st.stop)
     run()
