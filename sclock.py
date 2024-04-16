040df737db951ccf3f042adc4a1d2b0ebd8de7ee
SichaoY <13816901408@163.com>
2024-04-16 17:35:56 +0800

    extend sclock to primal screen and move some toggles to cfg.ini

--- sclock.py
+++ sclock.py
@@ -1,4 +1,6 @@
+import re
 from datetime import datetime
+import configparser
 import time
 from tkinter import *
 from stimer import add_leading_zero, splaysound
@@ -8,48 +10,75 @@ def gettime():
     now = datetime.now()
     return now.hour, now.minute, now.second
 
+
+def parse_ss(ss_str):
+    ret = re.match("(\d+)x(\d+)", ss_str)
+    if ret:
+        return int(ret[1]), int(ret[2])
+    else:
+        raise ValueError("screen_size format is not as 1920x1080, please check!")
+
+def resize(var_int, ratio):
+    return int(var_int/ratio)
+
 def main():
-    second_screen = "right"
-    sound_path_tick = "asset/audio/tick.mp3"
-    tick = True
+    config = configparser.ConfigParser()
+    config.read("cfg.ini")
+    second_screen = config.get("top", "second_screen_position")
+    sound_path_tick = config.get("top", "sound_path_tick")
+    tick = config.getboolean("top", "tick")
 
     root = Tk()
-    sw, sh = root.winfo_screenwidth(), root.winfo_screenheight()
-    print(f"screen1:\nscreenwidth: {sw}\nscreenheight: {sh}")
-
-
-    if sh == 1440:
-        font_size = 300
-        x_start = 100
-        y_shift = 300
-    else:
-        raise NotImplementedError
-
+    # the best way to check screen1 and 2's size is still to manually check in windows display setting
+    # sw1, sh1 = root.winfo_screenwidth(), root.winfo_screenheight()
+    sw1, sh1 = parse_ss(config.get("top", "screen1_size"))
+    sw2, sh2 = parse_ss(config.get("top", "screen2_size"))
+    
     if second_screen == "right":
-        root.geometry("%dx%d%+d+%d" % (sw, sh, sw, 0))
+        root.geometry("%dx%d%+d+%d" % (sw2, sh2, sw1, 0))
     elif second_screen == "left":
-        root.geometry("%dx%d%+d+%d" % (sw, sh, -sw, 0))
+        root.geometry("%dx%d%+d+%d" % (sw2, sh2, -sw1, 0))
+    elif second_screen == "no":
+        laptop_ratio = eval(config.get("top", "laptop_ratio"))
+        if laptop_ratio !=1:
+            sw1 = resize(sw1, laptop_ratio)
+            sh1 = resize(sh1, laptop_ratio)
+        sw2 = sw1
+        sh2 = sh1
+        root.geometry("%dx%d%+d+%d" % (sw2, sh2, 0, 0))
     else:
         raise NotImplementedError
+
+    print(f"screen2:\nscreenwidth: {sw2}\nscreenheight: {sh2}")
+
+    # default setting based on sh: 1440
+    font_size = 300
+    x_start = 100
+    y_shift = 300
+    # resize
+    if sh2 in [1080, 1200, 960]:
+        ratio = 1440/sh2
+        font_size = resize(font_size, ratio)
+        x_start = resize(x_start, ratio)
+        y_shift = resize(y_shift, ratio)
+    else:
+        raise NotImplementedError
+
     root.configure(background="#001")
 
-
-    x_shift = sw // 3
-    y_start = sh // 2 - y_shift
+    x_shift = sw2 // 3
+    y_start = sh2 // 2 - y_shift
     Font_tuple = ("Arial Black", font_size, "")
     hrs = StringVar()
-    Entry(
-        root, textvariable=hrs, width=2, font=Font_tuple, bg="#001", fg="#fff", bd=0
-    ).place(x=x_start, y=y_start)
+    Entry(root, textvariable=hrs, width=2, font=Font_tuple, bg="#001", fg="#fff", bd=0).place(x=x_start, y=y_start)
     mins = StringVar()
-    Entry(
-        root, textvariable=mins, width=2, font=Font_tuple, bg="#001", fg="#fff", bd=0
-    ).place(x=x_start + x_shift, y=y_start)
+    Entry(root, textvariable=mins, width=2, font=Font_tuple, bg="#001", fg="#fff", bd=0).place(
+        x=x_start + x_shift, y=y_start
+    )
     secs = StringVar()
-    Entry(
-        root, textvariable=secs, width=2, font=Font_tuple, bg="#001", fg="#fff", bd=0
-    ).place(x=x_start + x_shift * 2, y=y_start)
-
+    Entry(root, textvariable=secs, width=2, font=Font_tuple, bg="#001", fg="#fff", bd=0).place(
+        x=x_start + x_shift * 2, y=y_start
+    )
 
     def clocking():
         while True:
@@ -62,9 +91,9 @@ def main():
                 splaysound(sound_path_tick)
             time.sleep(1)
 
-
     Button(root, text="Start Clock", command=clocking).pack()
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
